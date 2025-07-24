27eb8a63e01973a35a949b6f70f590e74b535395
Anita Chivizhe <anita.chivize@sanlam.co.za>
2025-07-24 13:48:55 +0200

    Initial commit

---
+++ src/MsgList.js
@@ -0,0 +1,11 @@
+import { Msg } from "./Msg";
+export function MsgList() {
+  const names = ["Jeevan", "Ethan", "Siya", "Jamie"];
+  return (
+    <div>
+      {names.map((nm) => (
+        <Msg name={nm} />
+      ))}
+    </div>
+  );
+}
